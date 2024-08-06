2083fe124c0d3f086797a1d5363e4ab014c07ddb
Tahir <olawaletech2@gmail.com>
2024-08-06 18:02:56 +0100

    feat: get all user products

---
+++ src/Hng.Application/Features/Products/Dtos/GetUserProductsQueryParameters.cs
@@ -0,0 +1,10 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Hng.Application.Features.Products.Dtos
+{
+    public class GetUserProductsQueryParameters: GetProductsQueryParameters
+	{
+        [JsonPropertyName("user_id")]
+        public Guid UserId { get; set; }
+    }
+}
---
+++ src/Hng.Application/Features/Products/Queries/GetUserProductsQuery.cs
@@ -0,0 +1,18 @@
+﻿using Hng.Application.Features.Products.Dtos;
+using Hng.Application.Features.Subscriptions.Dtos.Requests;
+using Hng.Application.Features.Subscriptions.Dtos.Responses;
+using Hng.Application.Shared.Dtos;
+using MediatR;
+
+namespace Hng.Application.Features.Products.Queries
+{
+    public class GetUserProductsQuery : IRequest<PagedListDto<ProductDto>>
+    {
+        public GetUserProductsQuery(GetUserProductsQueryParameters parameters)
+        {
+            productsQueryParameters = parameters;
+        }
+
+        public GetUserProductsQueryParameters productsQueryParameters { get; set; }
+    }
+}
