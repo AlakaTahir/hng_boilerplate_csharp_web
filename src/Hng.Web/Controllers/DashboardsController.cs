4996df26bd6058434b5faf8a62843a2798704ac3
Tahir <olawaletech2@gmail.com>
2024-08-14 19:47:03 +0100

    feat: update version

--- src/Hng.Web/Controllers/DashboardsController.cs
+++ src/Hng.Web/Controllers/DashboardsController.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Hng.Web.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/v1/[controller]")]
     [ApiController]
     public class DashboardsController : ControllerBase
     {
